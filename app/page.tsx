54cdf1901f2a9c14e68a175280bb817db64b2826
kriskramer94 <kriskramer.developer@gmail.com>
2025-09-05 20:16:20 -0600

    Finished

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import CustomFileInput from "@/components/CustomFileInput";
+import FileInput from "@/components/FileInput";
+
+export default function Home() {
+  return (
+    <div className="p-10">
+      {/* <FileInput /> */}
+      <CustomFileInput />
+    </div>
+  );
+}
---
+++ components/FileInput.tsx
@@ -0,0 +1,23 @@
+import classNames from "classnames";
+import React, { ComponentPropsWithRef } from "react";
+
+type Props = ComponentPropsWithRef<"input">;
+const FileInput = (props: Props) => {
+  return (
+    <input
+      {...props}
+      className={classNames({
+        // button colors
+        "file:bg-violet-50 file:text-violet-500 hover:file:bg-violet-100": true,
+        // button shape and spacing
+        "file:rounded-lg file:rounded-tr-none file:rounded-br-none": true,
+        "file:px-4 file:py-2 file:mr-4 file:border-none": true,
+        // overall input styling
+        "hover:cursor-pointer border rounded-lg text-gray-400": true,
+      })}
+      type="file"
+    />
+  );
+};
+
+export default FileInput;
